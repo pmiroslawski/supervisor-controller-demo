053975669a142c9398595e0c83f1e8af9ae6cd8b
Paweł Mirosławski <pmiroslawski@gmail.com>
2021-08-25 11:31:02 +0000

    events notifications added

--- src/EventSubscriber/SupervisorSubscriber.php
+++ src/EventSubscriber/SupervisorSubscriber.php
@@ -7,9 +7,35 @@ use Bit9\SupervisorControllerBundle\Event\ProcessStartedEvent;
 use Bit9\SupervisorControllerBundle\Event\ProcessStoppedEvent;
 use Bit9\SupervisorControllerBundle\Event\ProcessesStartedEvent;
 use Bit9\SupervisorControllerBundle\Event\ProcessesStoppedEvent;
+use Symfony\Component\Notifier\Message\ChatMessage;
+use Symfony\Component\Notifier\ChatterInterface;
+use Symfony\Component\Notifier\Bridge\Telegram\Reply\Markup\InlineKeyboardMarkup;
+use Symfony\Component\Notifier\Bridge\Telegram\TelegramOptions;
 
 class SupervisorSubscriber implements EventSubscriberInterface
 {
+    private ChatterInterface $chatter;
+
+    public function __construct(ChatterInterface $chatter)
+    {
+        $this->chatter = $chatter;
+    }
+
+    private function notify(string $text)
+    {
+        $telegramOptions = (new TelegramOptions())
+            ->parseMode(TelegramOptions::PARSE_MODE_HTML)
+            ->disableWebPagePreview(true)
+            ->replyMarkup((new InlineKeyboardMarkup()))
+        ;
+
+        $message = (new ChatMessage($text, $telegramOptions))
+            ->transport('telegram')
+        ;
+
+        return $this->chatter->send($message);
+    }
+
     public static function getSubscribedEvents(): array
     {
         return [
@@ -22,21 +48,34 @@ class SupervisorSubscriber implements EventSubscriberInterface
 
     public function onProcessStarted(ProcessStartedEvent $event)
     {
-//         dump($event);
+        $text = "[Supervisor Watchdog Service] ";
+        $text .= sprintf("started process %s", $event->getProcessName());
+
+        $this->notify($text);
+
     }
 
     public function onProcessStopped(ProcessStoppedEvent $event)
     {
-//         dump($event);
+        $text = "[Supervisor Watchdog Service] ";
+        $text .= sprintf("stopped process %s", $event->getProcessName());
+
+        $this->notify($text);
     }
 
     public function onProcessesStarted(ProcessesStartedEvent $event)
     {
-//         dump($event);
+        $text = "[Supervisor Watchdog Service] ";
+        $text .= sprintf("Increased number of consumers for group %s \nCurrently %d %s running.", $event->getProgramName(), $event->getProcessesNum(), $event->getProcessesNum() == 1 ? 'consumer is' : 'consumers are');
+
+        $this->notify($text);
     }
 
     public function onProcessesStopped(ProcessesStoppedEvent $event)
     {
-//         dump($event);
+        $text = "[Supervisor Watchdog Service] ";
+        $text .= sprintf("Decreased number of consumers for group %s \nCurrently %d consumers %s running.", $event->getProgramName(), $event->getProcessesNum(), $event->getProcessesNum() == 1 ? 'consumer is' : 'consumers are');
+
+        $this->notify($text);
     }
 }
